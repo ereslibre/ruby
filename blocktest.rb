30365c5cea7f4753626d04733bb2c1cedd20a5ad
Rafael Fernández López <ereslibre@ereslibre.es>
2010-05-24 08:36:32 +0200

    A great example to show the power of yield/lambda expressions

---
+++ blocktest.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require "pp"
+
+def fibUpTo(max)
+	n1, n2 = 1, 1
+	while n1 <= max
+		yield n1
+		n1, n2 = n2, n1 + n2
+	end
+end
+
+def into(array)
+	return proc { |val| array << val }
+end
+
+fibUpTo 50, &into(a = [])
+pp a
