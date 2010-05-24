ea3b5b2c76af897736cc841dc60d00f0c9ef0127
Rafael Fernández López <ereslibre@ereslibre.es>
2010-05-24 09:17:37 +0200

    method

---
+++ method.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+class Array
+  def iterate!(code)
+    self.each_with_index do |n, i|
+      self[i] = code.call(n)
+    end
+  end
+end
+
+def square(n)
+  n ** 2
+end
+
+array = [1, 2, 3, 4]
+
+array.iterate!(method(:square))
+
+puts array.inspect
